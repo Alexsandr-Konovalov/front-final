973417eb84988d7ae06fda19ddd8449c0da8c4b3
Regem7 <regem7@gmail.com>
2024-02-26 19:51:06 +0500

    add TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ToDoList } from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className='App-header'>
+        <ToDoList />
       </header>
     </div>
   );
